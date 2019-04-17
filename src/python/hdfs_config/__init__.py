946977c311026d6b1a955dd3478db5e93d7bd9a0
phongdk <phongdk@coccoc.com>
2019-04-17 16:14:13 +0700

    Using Dask to read & write data to HDFS

---
+++ src/python/hdfs_config/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Apr 12 10:33 2019
+
+@author: phongdk
+"""
---
+++ src/python/hdfs_config/hdfs_config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Apr 12 10:33 2019
+
+@author: phongdk
+"""
+
+import hdfs3
+
+storage_options = {'host': 'hdfs://ads-target1v.dev.itim.vn',
+                   'port': 8020,
+                   'dfs.domain.socket.path': '/home/phuongdv/hadoopdata/hadoop-hdfs/dn_socket',
+                   'user': 'phongdk'}
+
+hdfs = hdfs3.HDFileSystem('ads-target1v.dev.itim.vn', port=8020,
+                          pars={'dfs.domain.socket.path': '/home/phuongdv/hadoopdata/hadoop-hdfs/dn_socket'})
